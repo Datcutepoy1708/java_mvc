02ebd673f26ed646cf0693bc68ad977e2ad7f6a0
Trong Dat <dtcutepoy1708@gmail.com>
2026-02-27 07:54:01 +0700

    register DTO

--- demo/src/main/java/com/example/demo/controller/client/HomePageController.java
+++ demo/src/main/java/com/example/demo/controller/client/HomePageController.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.domain.Product;
+import com.example.demo.domain.dto.RegisterDTO;
 import com.example.demo.service.ProductService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -28,6 +33,13 @@ public class HomePageController {
     }
     @GetMapping(value = "/register")
     public String getRegisterPage(Model model){
+        model.addAttribute("registerUser", new RegisterDTO());
         return "client/auth/register";
-    }    
+    }
+    
+    @PostMapping("/register")
+    public String handleRegister(@ModelAttribute("registerUser") RegisterDTO registerDTO ){
+        return "client/auth/register";
+    }
+    
 }
