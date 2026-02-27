bcd9e20a7a3597828cbff8ac0d094c75cf0c250b
Trong Dat <dtcutepoy1708@gmail.com>
2026-02-27 14:44:23 +0700

    complete register screen

--- demo/src/main/java/com/example/demo/service/validator/RegisterValidator.java
+++ demo/src/main/java/com/example/demo/service/validator/RegisterValidator.java
@@ -1,11 +1,21 @@
 package com.example.demo.service.validator;
 
+import org.springframework.stereotype.Service;
+
 import com.example.demo.domain.dto.RegisterDTO;
+import com.example.demo.service.UserService;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+@Service
 public class RegisterValidator implements ConstraintValidator<RegisterCheck,RegisterDTO> {
+    
+    private final UserService userService;
+    public RegisterValidator(UserService userService){
+      this.userService=userService;
+    }
+
     @Override
     public boolean isValid(RegisterDTO user,ConstraintValidatorContext context){
         boolean valid=true;
@@ -19,6 +29,15 @@ public class RegisterValidator implements ConstraintValidator<RegisterCheck,Regi
             .disableDefaultConstraintViolation();
           valid=false;  
         }
+        //check email
+        if(this.userService.checkEmailExist(user.getEmail())){
+          context.buildConstraintViolationWithTemplate("The email already exists")
+          .addPropertyNode("email")
+          .addConstraintViolation()
+          .disableDefaultConstraintViolation();
+          valid=false;
+        }
+
         return valid;
     }
 }
