f7fc1635b29e713ba3742aca3ff09fd02f8d54ea
Trong Dat <dtcutepoy1708@gmail.com>
2026-03-08 22:51:56 +0700

    spring session

--- demo/src/main/java/com/example/demo/config/SecurityConfiguration.java
+++ demo/src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -7,10 +7,12 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.session.security.web.authentication.SpringSessionRememberMeServices;
 
 import com.example.demo.service.CustomUserDetailsService;
 import com.example.demo.service.UserService;
@@ -64,6 +66,14 @@ DispatcherType.INCLUDE) .permitAll()
 "/images/**").permitAll()
     .requestMatchers("/admin/**").hasRole("ADMIN")
     .anyRequest().authenticated())
+    .sessionManagement((sessionManagement) -> sessionManagement
+.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+
+.invalidSessionUrl("/logout?expired")
+.maximumSessions(1)
+.maxSessionsPreventsLogin(false))
+.logout(logout->logout.deleteCookies("JSESSIONID").invalidateHttpSession(true))
+    .rememberMe(r -> r.rememberMeServices(rememberMeServices()))
     .formLogin(formLogin -> formLogin
     .loginPage("/login")
     .failureUrl("/login?error")
@@ -72,4 +82,15 @@ DispatcherType.INCLUDE) .permitAll()
     ).exceptionHandling(ex->ex.accessDeniedPage("/access-deny"));
     return http.build();
     }
+
+    @Bean
+public SpringSessionRememberMeServices rememberMeServices() {
+SpringSessionRememberMeServices rememberMeServices =
+new SpringSessionRememberMeServices();
+
+// optionally customize
+rememberMeServices.setAlwaysRemember(true);
+return rememberMeServices;
 }
+}
+
